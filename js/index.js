7d647359ff3fbb3c57300af84638dc11bda52c7d
matiaranda099 <matiaranda426@live.com.ar>
2022-06-22 21:49:58 -0300

    Simulador Interactivo

--- js/index.js
+++ js/index.js
@@ -1,40 +1,57 @@
-
-
-//Pide una nota (número). Muestra la calificación según la nota
-
-let nota = prompt("Ingresa tu nota: ");
-if (nota > 0 && nota <= 10) {
- if (nota < 3){ 
-  alert("Tu nonta es: " +" Deficiente " +  nota);
-  } else if (nota < 5){
-    alert("Tu nota es: " +"Insuficiente " +  nota );
-  } else if (nota < 6){ 
-    alert("Tu nota es: " + "Insuficiente " +  nota);
-  } else if (nota < 7){
-    alert("Tu nota es: " +"Aprobado "  +  nota);
-  } else if (nota < 8){
-    alert("Tu nota es: " +"Bueno " +  nota );
-  } else if (nota < 9){
-    alert("Tu nota es: " +"Muy Bueno " +  nota);
-  } else if (nota >= 9){
-    alert("Tu nota es: " +"SOBRESALIENTE!! " +  nota);
-  }
+//Funcion para el saludo
+function saludo() {
+    nombre = prompt("Ingrese su nombre completo: ");
+    while (nombre === "") {
+        nombre = prompt("Ingrese su nombre completo: ");
+    }
+    alert(nombre + " acá encontrarás productos #FREEANIMAL y una forma distinta de incluir vegetales a tu dieta! Bienvenido/a");
 }
-nota = prompt("Ingresa una nueva nota")
-if (nota > 0 && nota <= 10) {
-  if (nota < 3){ 
-   alert("Tu nonta es: " +" Deficiente " +  nota);
-   } else if (nota < 5){
-     alert("Tu nota es: " +"Insuficiente " +  nota );
-   } else if (nota < 6){ 
-     alert("Tu nota es: " + "Insuficiente " +  nota);
-   } else if (nota < 7){
-     alert("Tu nota es: " +"Aprobado "  +  nota);
-   } else if (nota < 8){
-     alert("Tu nota es: " +"Bueno " +  nota );
-   } else if (nota < 9){
-     alert("Tu nota es: " +"Muy Bueno " +  nota);
-   } else if (nota >= 9){
-     alert("Tu nota es: " +"SOBRESALIENTE!! " +  nota);
-   }
- }
+
+
+//Funcion de agregar los productos al carrito
+function productoSalados() {
+    mensaje = prompt("Escojé  algunos de estos productos: \n 1)Medallones \n 2)Milanesas \n 3)Empanadas");
+    if (mensaje === "1") {
+        alert("Comprás Medallones");
+    } else if (mensaje === "2") {
+        alert("Comprás Milanesas");
+    } else if (mensaje === "3") {
+        alert("Comprás Empanadas");
+    }
+    opcion = prompt(
+        "vuelva a ingresar una opcion: \n 2)Mostrar monto total \n 3)Finalizamos por hoy!");
+}
+
+//Hago la funcion para terminar la compra y mostrar los precios
+function terminarCompra() {
+    if (mensaje === "1") {
+        alert("Elegiste Medallones a un precio total de: $" + medallones);
+    } else if (mensaje === "2") {
+        alert("Elegiste Milanesas a un precio de: $" + milanesas);
+    } else if (mensaje === "3") {
+        alert("Elegiste Empanadas a un precio de : $" + empanadas);
+    }
+}
+
+//Declaro algunas variables sin inicializar
+let mensaje;
+let medallones = 450;
+let milanesas = 550;
+let empanadas = 650;
+
+let nombre;
+saludo();
+let opcion = prompt(
+    "ingrese una opcion: \n 1)Mostrar los productos \n 2)Terminar la compra  \n 3)Finalizamos por hoy! "
+);
+while (opcion !== "3") {
+    if (opcion === "1") {
+        productoSalados();
+    }
+    if (opcion === "2") {
+        terminarCompra();
+        opcion = "3";
+    }
+}
+
+alert("Gracias por salvar a la vida Animal");
